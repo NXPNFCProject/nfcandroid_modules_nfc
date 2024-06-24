9ed5356875498614e6cccf76cb6b466ebc436f91
nxf89829 <bhanusurendra.narra@nxp.com>
2024-06-24 18:54:25 +0530

    [artf1150961]: HAL Request Control Enablement in Android-16
    Supporting HAL Request and release control API
    for AIDL2.0 onwards
    
    Change-Id: I9744857b07d30223d4aa96eba5f4bae26b0670aa

--- src/adaptation/NfcAdaptation.cc
+++ src/adaptation/NfcAdaptation.cc
@@ -89,6 +89,7 @@ INfcClientCallback* NfcAdaptation::mCallback;
 std::shared_ptr<INfcAidlClientCallback> mAidlCallback;
 ::ndk::ScopedAIBinder_DeathRecipient mDeathRecipient;
 std::shared_ptr<INfcAidl> mAidlHal;
+int32_t mAidlHalVer;
 
 bool nfc_nci_reset_keep_cfg_enabled = false;
 uint8_t nfc_nci_reset_type = 0x00;
@@ -224,6 +225,12 @@ class NfcAidlClientCallback
         break;
       case NfcAidlEvent::HCI_NETWORK_RESET:
         e_num = HAL_HCI_NETWORK_RESET;
+        break;
+      case NfcAidlEvent::REQUEST_CONTROL:
+        e_num = HAL_NFC_REQUEST_CONTROL_EVT;
+        break;
+      case NfcAidlEvent::RELEASE_CONTROL:
+        e_num = HAL_NFC_RELEASE_CONTROL_EVT;
         break;
       case NfcAidlEvent::ERROR:
       default:
@@ -721,7 +728,9 @@ void NfcAdaptation::InitializeHalDeviceContext() {
       AIBinder_linkToDeath(mAidlHal->asBinder().get(), mDeathRecipient.get(),
                            this /* cookie */);
       mHal = mHal_1_1 = mHal_1_2 = nullptr;
-      LOG(INFO) << StringPrintf("%s: INfcAidl::fromBinder returned", func);
+      mAidlHal->getInterfaceVersion(&mAidlHalVer);
+      LOG(INFO) << StringPrintf("%s: INfcAidl::fromBinder returned ver(%d)",
+                                func, mAidlHalVer);
     }
     LOG_ALWAYS_FATAL_IF(mAidlHal == nullptr,
                         "Failed to retrieve the NFC AIDL!");
@@ -919,7 +928,12 @@ void NfcAdaptation::HalControlGranted() {
   const char* func = "NfcAdaptation::HalControlGranted";
   LOG(DEBUG) << StringPrintf("%s", func);
   if (mAidlHal != nullptr) {
-    LOG(ERROR) << StringPrintf("Unsupported function %s", func);
+    if (mAidlHalVer > 1) {
+      NfcAidlStatus aidl_status;
+      mAidlHal->controlGranted(&aidl_status);
+    } else {
+      LOG(ERROR) << StringPrintf("Unsupported function %s", func);
+    }
   } else if (mHal != nullptr) {
     mHal->controlGranted();
   }
