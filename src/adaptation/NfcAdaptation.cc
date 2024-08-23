41b14e9b77799b3983d29cacc384a77fa1304f90
nxf71239 <krnaveen.kumar@nxp.com>
2024-08-23 14:37:31 +0530

    [artf1170715] : Enabling T4T based on vendor property
    - Added NAME_T4T_NFCEE_ENABLE property to enable/disable the T4T features
    
    Change-Id: I369db98c1964a0a2e46637e660cec72a92b6cf13

--- src/adaptation/NfcAdaptation.cc
+++ src/adaptation/NfcAdaptation.cc
@@ -372,6 +372,8 @@ void NfcAdaptation::GetVendorConfigs(
         ConfigValue((uint8_t)aidlConfigValue.defaultSystemCodePowerState));
     configMap.emplace(NAME_OFF_HOST_ESE_PIPE_ID,
                       ConfigValue((uint8_t)aidlConfigValue.offHostESEPipeId));
+    configMap.emplace(NAME_T4T_NFCEE_ENABLE,
+                      ConfigValue((uint8_t)aidlConfigValue.t4tNfceeEnable));
 
     if (aidlConfigValue.offHostSIMPipeIds.size() != 0) {
       configMap.emplace(NAME_OFF_HOST_SIM_PIPE_IDS,
