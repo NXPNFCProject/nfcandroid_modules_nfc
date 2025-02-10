f583358196eade13348ea48d0b9521e2b724d3ff
Sai Shweta S <sai.shwethas@nxp.com>
2025-02-10 18:21:11 +0100

    Revert "[artf1207623]: Reverting NXP changes to AOSP change"
    This reverts commit 83bbe0212e416de2e5b56ce49b93607c299d3380.
    
    Reason for revert: <Suspect feature break>
    
    Change-Id: Ia7441ccef6f836f8407247968c2adf12bda883cd

--- NfcNci/src/com/android/nfc/NfcService.java
+++ NfcNci/src/com/android/nfc/NfcService.java
@@ -1786,6 +1786,11 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                 watchDog.cancel();
             }
 
+        if (mIsHceCapable) {
+        // Generate the initial card emulation routing table
+        mCardEmulationManager.onNfcEnabled();
+        }
+
             mSkipNdefRead = NfcProperties.skipNdefRead().orElse(false);
             nci_version = getNciVersion();
             Log.d(TAG, "NCI_Version: " + nci_version);
@@ -1837,11 +1842,6 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                         && mAlwaysOnState != NfcAdapter.STATE_TURNING_OFF)) {
                 /* Start polling loop */
                 applyRouting(true);
-            }
-
-            if (mIsHceCapable) {
-                // Generate the initial card emulation routing table
-                mCardEmulationManager.onNfcEnabled();
             }
 
             if (mIsRecovering) {
