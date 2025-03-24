7c5a32617f3b3189c7a1e020cde6a146039f997c
nxf97491 <ramyesh.thanthri@nxp.com>
2025-03-24 16:19:12 +0530

    Test: Adding test cases for HostApduService
    Bug: 372488488
    
    atest android.nfc:
    "summary": {"PASSED": 366, "FAILED": 0, "IGNORED": 0}}}}, "total_summary": {"PASSED": 366, "FAILED": 0, "IGNORED": 0}}
    
    Test Result:
    {"args": "android.nfc.cardemulation.HostApduServiceTest --serial=localhost:44237", "test_runner": {"AtestTradefedTestRunner": {"arm64-v8a NfcManagerTests": {"PASSED": [{"test_name": "android.nfc.cardemulation.HostApduServiceTest#testHandleMessageWithMsgDeactivated", "test_time": "(379ms)", "details": null}, {"test_name": "android.nfc.cardemulation.HostApduServiceTest#testHandleMessageWithApduAck", "test_time": "(26ms)", "details": null}, {"test_name": "android.nfc.cardemulation.HostApduServiceTest#testHandleMessageWithResponseApduWhenServiceDeactivated", "test_time": "(1ms)", "details": null}, {"test_name": "android.nfc.cardemulation.HostApduServiceTest#testNfcService", "test_time": "(26ms)", "details": null}, {"test_name": "android.nfc.cardemulation.HostApduServiceTest#testHandleMessageWithPollingLoop", "test_time": "(26ms)", "details": null}, {"test_name": "android.nfc.cardemulation.HostApduServiceTest#testHandleMessageWithResponseApdu", "test_time": "(27ms)", "details": null}, {"test_name": "android.nfc.cardemulation.HostApduServiceTest#testHandleMessageWithCmdApdu", "test_time": "(51ms)", "details": null}, {"test_name": "android.nfc.cardemulation.HostApduServiceTest#testHandleMessageWithMsgUnhandled", "test_time": "(27ms)", "details": null}, {"test_name": "android.nfc.cardemulation.HostApduServiceTest#testHandleMessageWithMsgUnhandledWhenServiceDeactivated", "test_time": "(0ms)", "details": null}, {"test_name": "android.nfc.cardemulation.HostApduServiceTest#testOnBinder", "test_time": "(102ms)", "details": null}], "summary": {"PASSED": 10, "FAILED": 0, "IGNORED": 0}}}}, "total_summary": {"PASSED": 10, "FAILED": 0, "IGNORED": 0}}
    URL: https://android-review.googlesource.com/c/platform/packages/modules/Nfc/+/3507461

--- framework/tests/src/android/nfc/cardemulation/HostApduServiceTest.java
+++ framework/tests/src/android/nfc/cardemulation/HostApduServiceTest.java
@@ -16,24 +16,39 @@
 
 package android.nfc.cardemulation;
 
+import static android.nfc.cardemulation.HostApduService.MSG_RESPONSE_APDU;
+import static android.nfc.cardemulation.HostApduService.MSG_UNHANDLED;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import android.content.Intent;
+import android.nfc.Flags;
 import android.os.Bundle;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 
+import com.android.dx.mockito.inline.extended.ExtendedMockito;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
+import org.mockito.MockitoSession;
+import org.mockito.quality.Strictness;
 
 public class HostApduServiceTest {
     private byte[] sampleApdu = "EO00001800".getBytes();
     private HostApduService.MsgHandler mHandler;
     private SampleHostApduService mSampleService;
+    private MockitoSession mMockitoSession;
 
     class SampleHostApduService extends HostApduService {
 
@@ -50,12 +65,22 @@ public class HostApduServiceTest {
 
     @Before
     public void setUp() {
+        mMockitoSession = ExtendedMockito.mockitoSession()
+                .mockStatic(Flags.class)
+                .mockStatic(com.android.nfc.module.flags.Flags.class)
+                .strictness(Strictness.LENIENT)
+                .startMocking();
         MockitoAnnotations.initMocks(this);
         if (Looper.myLooper() == null) {
             Looper.prepare();
         }
         mSampleService = mock(SampleHostApduService.class, CALLS_REAL_METHODS);
         mHandler = mSampleService.getMsgHandler();
+    }
+
+    @After
+    public void tearDown() {
+        mMockitoSession.finishMocking();
     }
 
     @Test
@@ -74,4 +99,113 @@ public class HostApduServiceTest {
         });
     }
 
+    @Test
+    public void testHandleMessageWithResponseApduWhenServiceDeactivated() {
+        Message msg = mock(Message.class);
+        msg.what = MSG_RESPONSE_APDU;
+
+        when(mSampleService.getNfcService()).thenReturn(null);
+        mHandler.post(() -> {
+            mHandler.handleMessage(msg);
+            verify(mSampleService).getNfcService();
+        });
+    }
+
+    @Test
+    public void testHandleMessageWithResponseApdu() {
+        Message msg = mock(Message.class);
+        Messenger mNfcService = mock(Messenger.class);
+        msg.what = MSG_RESPONSE_APDU;
+        msg.replyTo = mNfcService;
+
+        when(mSampleService.getNfcService()).thenReturn(mNfcService);
+        mHandler.post(() -> {
+            mHandler.handleMessage(msg);
+            verify(mSampleService).getMessenger();
+            try {
+                verify(mNfcService).send(msg);
+            } catch (RemoteException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    @Test
+    public void testHandleMessageWithApduAck() {
+        Message msg = mock(Message.class);
+        msg.what = HostApduService.MSG_RESPONSE_APDU_ACK;
+
+        mHandler.post(() -> {
+            mHandler.handleMessage(msg);
+            verify(mSampleService, never()).getNfcService();
+        });
+    }
+
+    @Test
+    public void testHandleMessageWithMsgDeactivated() {
+        Message msg = Message.obtain();
+        msg.what = HostApduService.MSG_DEACTIVATED;
+        msg.arg1 = 1;
+
+        mHandler.post(() -> {
+            mHandler.handleMessage(msg);
+            verify(mSampleService).setNfcService(null);
+            verify(mSampleService).onDeactivated(1);
+        });
+    }
+
+    @Test
+    public void testHandleMessageWithMsgUnhandledWhenServiceDeactivated() {
+        Message msg = mock(Message.class);
+        msg.what = MSG_UNHANDLED;
+        when(mSampleService.getNfcService()).thenReturn(null);
+
+        mHandler.post(() -> {
+            mHandler.handleMessage(msg);
+            verify(mSampleService, only()).getNfcService();
+        });
+    }
+
+    @Test
+    public void testHandleMessageWithMsgUnhandled() {
+        Message msg = mock(Message.class);
+        msg.what = MSG_UNHANDLED;
+        Messenger mNfcService = mock(Messenger.class);
+        when(mSampleService.getNfcService()).thenReturn(mNfcService);
+
+        mHandler.post(() -> {
+            mHandler.handleMessage(msg);
+            verify(mSampleService).getMessenger();
+        });
+    }
+
+    @Test
+    public void testHandleMessageWithPollingLoop() {
+        Message msg = mock(Message.class);
+        msg.what = HostApduService.MSG_POLLING_LOOP;
+        when(Flags.nfcReadPollingLoop()).thenReturn(true);
+        when(com.android.nfc.module.flags.Flags.nfcHceLatencyEvents()).thenReturn(true);
+
+        mHandler.post(() -> {
+            mHandler.handleMessage(msg);
+            verify(mSampleService).getMessenger();
+        });
+    }
+
+    @Test
+    public void testOnBinder() {
+        Messenger messenger = mock(Messenger.class);
+        when(mSampleService.getMessenger()).thenReturn(messenger);
+
+        mSampleService.onBind(mock(Intent.class));
+        verify(messenger).getBinder();
+    }
+
+    @Test
+    public void testNfcService() {
+        Messenger nfcService = mock(Messenger.class);
+        mSampleService.setNfcService(nfcService);
+
+        assertEquals(nfcService, mSampleService.getNfcService());
+    }
 }
