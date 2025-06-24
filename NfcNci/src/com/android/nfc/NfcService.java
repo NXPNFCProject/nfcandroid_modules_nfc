ed1591f58212e1c91c1292ebd8e68612b7de2e90
Himanshu Singh Kushwah <himanshusingh.kushwah@nxp.com>
2025-06-24 10:10:26 +0530

    [artf1213992][AOSP] : Reinit NFC stack if the OEM extension dies.
    When the OEM extension dies, restart NFC stack to bring it to a stable state.
    
    Test: Compiles
    Bug: 422706337
    Change-Id: I11f6628f1413553f83996c42fb13c13db75b5e31
    Flag: EXEMPT bug fix

--- NfcNci/src/com/android/nfc/NfcService.java
+++ NfcNci/src/com/android/nfc/NfcService.java
@@ -3658,6 +3658,7 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
         synchronized (NfcService.this) {
             Log.w(TAG, "binderDied: OEM extension died");
             mNfcOemExtensionCallback = null;
+            restartStack();
         }
     };
 
