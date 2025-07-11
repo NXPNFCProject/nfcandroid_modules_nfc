d8b759776e3f8be1483fbb4fe5a30238fe7e935b
nxf24591 <nanjesh.s_1@nxp.com>
2025-07-11 16:47:58 +0530

    [artf1265062][25Q4_AUTO]: nfc: Adjusting NCI Frame annotation command
    The technology bits of command is different in the HAL implementation compared
    
    to what the framework has implemented.
    
    Bug: 409095574
    
    Test: Manual CTS verifier tests
    
    Flag: EXEMPT bug fix
    
    (cherry picked from https://android-review.googlesource.com/q/commit:825b83d787e654f2577df199f955dd2cd2f8f1f5)
    
    Merged-In: Ic9ed821265f2b420d1ef92fc6c0348b77de98a9d

--- NfcNci/nci/jni/NativeNfcManager.cpp
+++ NfcNci/nci/jni/NativeNfcManager.cpp
@@ -1809,12 +1809,12 @@ static tNFA_STATUS setTechAPollingLoopAnnotation(JNIEnv* env, jobject o,
       command.push_back(0x00);
     } else {
       command.push_back(0x01);                 // Number of frame entries.
-      command.push_back(0x21);                 // Position and type.
+      command.push_back(0x20);                 // Position and type.
       command.push_back(annotation_size + 3);  // Length
       command.push_back(0x0a);                 // Waiting time
       command.insert(command.end(), annotation_data, annotation_data + annotation_size);
-      command.push_back(0x00);
-      command.push_back(0x00);
+      command.push_back(0x00);                 // Dummy bytes for HAL to populate CRC.
+      command.push_back(0x00);                 // Dummy bytes for HAL to populate CRC.
     }
     SyncEventGuard guard(gNfaVsCommand);
     tNFA_STATUS status =
