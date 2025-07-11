6abdd7f02cd4ea19647f8d3b2f5fdc408dd72674
nxf24591 <nanjesh.s_1@nxp.com>
2025-07-11 17:59:40 +0530

    [artf1265062][25Q4_AUTO]: Only keep last MSG_PREFERRED_PAYMENT_CHANGED
    Bug: 419458298
    
    Test: Compiles
    
    Flag: EXEMPT bugfix

--- NfcNci/src/com/android/nfc/NfcService.java
+++ NfcNci/src/com/android/nfc/NfcService.java
@@ -4896,6 +4896,7 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
     }
 
     public void onPreferredPaymentChanged(int reason) {
+        mHandler.removeMessages(MSG_PREFERRED_PAYMENT_CHANGED);
         sendMessage(MSG_PREFERRED_PAYMENT_CHANGED, reason);
     }
 
