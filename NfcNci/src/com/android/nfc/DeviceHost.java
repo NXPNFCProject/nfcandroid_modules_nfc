b83df1a32a5022927d9c82500373caccbc6c8945
nxf24591 <nanjesh.s_1@nxp.com>
2025-07-11 17:58:39 +0530

    R/W improvements Check if shutting down to end presence checking
    Can cause a crash if pres check cmd received while HAL is
    
    closing and no RSP is received
    
    Bug: 404481264
    
    (cherry picked from https://partner-android-review.googlesource.com/q/commit:b4fa45786c9cacbdee82648b4f0a77731aa1d804)
    
    Test: TagIOT/Stability
    
    Flag: EXEMPT bug fix
    
    Merged-In: I463c6f3a3e8e8e567e20913ed27be1ea7db5f74c

--- NfcNci/src/com/android/nfc/DeviceHost.java
+++ NfcNci/src/com/android/nfc/DeviceHost.java
@@ -85,7 +85,7 @@ public interface DeviceHost {
         boolean isPresent();
         void startPresenceChecking(int presenceCheckDelay,
                                    @Nullable TagDisconnectedCallback callback);
-        void stopPresenceChecking();
+        void stopPresenceChecking(boolean isShutdown);
         boolean isPresenceCheckStopped();
         void prepareForRemovalDetectionMode();
 
