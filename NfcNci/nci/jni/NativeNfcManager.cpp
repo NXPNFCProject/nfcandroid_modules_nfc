16e6c15270b80d2932c4552e65a9086a642be516
nxf24591 <nanjesh.s_1@nxp.com>
2025-07-11 16:47:11 +0530

    nfc: set initial screen state to UNKNOWN
    Bug: 409943151
    
    Test: Manual
    
    Flag: EXEMPT bug fix

--- NfcNci/nci/jni/NativeNfcManager.cpp
+++ NfcNci/nci/jni/NativeNfcManager.cpp
@@ -200,7 +200,7 @@ tNFA_STATUS gVSCmdStatus = NFA_STATUS_OK;
 uint16_t gCurrentConfigLen;
 uint8_t gConfig[256];
 std::vector<uint8_t> gCaps(0);
-static int prevScreenState = NFA_SCREEN_STATE_OFF_LOCKED;
+static int prevScreenState = NFA_SCREEN_STATE_UNKNOWN;
 static int NFA_SCREEN_POLLING_TAG_MASK = 0x10;
 bool gIsDtaEnabled = false;
 static bool gObserveModeEnabled = false;
@@ -1712,7 +1712,7 @@ static jboolean nfcManager_doInitialize(JNIEnv* e, jobject o) {
           }
         }
 
-        prevScreenState = NFA_SCREEN_STATE_OFF_LOCKED;
+        prevScreenState = NFA_SCREEN_STATE_UNKNOWN;
 
         // Do custom NFCA startup configuration.
         doStartupConfig();
