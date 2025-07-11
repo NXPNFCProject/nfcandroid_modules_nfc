a47e6766afce7cf27b077e0c1d59fd823e16b082
nxf24591 <nanjesh.s_1@nxp.com>
2025-07-11 18:00:36 +0530

    [artf1265062][25Q4_AUTO]: [service] Fix unnecessary hci broadcasts
    Upstream from Samsung + some cleanup
    
    Bug: 424897070
    
    Test: CtsNfcHceMultiDeviceTestCases
    
    Flag: EXEMPT bugfix

--- NfcNci/src/com/android/nfc/NfcService.java
+++ NfcNci/src/com/android/nfc/NfcService.java
@@ -17,6 +17,8 @@
 package com.android.nfc;
 
 import static android.Manifest.permission.BIND_NFC_SERVICE;
+import static android.Manifest.permission.NFC_PREFERRED_PAYMENT_INFO;
+import static android.Manifest.permission.NFC_TRANSACTION_EVENT;
 import static android.content.Intent.ACTION_BOOT_COMPLETED;
 import static android.content.Intent.ACTION_LOCKED_BOOT_COMPLETED;
 import static android.nfc.OemLogItems.EVENT_DISABLE;
@@ -1582,6 +1584,24 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
         }
     }
 
+    /**
+     * If a package requested a permission and package's sharedUserId is {@code android.uid.system},
+     * the other packages having the same sharedUserId are considered to request the permission. So
+     * {@code PackageManager::getPackagesHoldingPermissions} returns these packages even though they
+     * don't request the permission. We need to check permissions of returned packages separately.
+     */
+    private List<String> getPackagesHoldingPermission(PackageManager pm, String permission) {
+        List<String> packages = pm.getPackagesHoldingPermissions(
+                new String[] { permission }, PackageManager.GET_PERMISSIONS)
+                .stream()
+                .filter((pkg) -> pkg.requestedPermissions != null
+                        && Arrays.asList(pkg.requestedPermissions).contains(permission))
+                .map((pkg) -> pkg.packageName)
+                .toList();
+        Log.d(TAG, "got " + packages.size() + " packages holding permission " + permission);
+        return packages;
+    }
+
     void updatePackageCache() {
         UserManager um = mContext.createContextAsUser(
                 UserHandle.of(ActivityManager.getCurrentUser()), /*flags=*/0)
@@ -1602,26 +1622,12 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                     continue;
                 }
 
-                List<PackageInfo> packagesNfcEvents = pm.getPackagesHoldingPermissions(
-                        new String[] {android.Manifest.permission.NFC_TRANSACTION_EVENT},
-                        0);
-                List<PackageInfo> packagesNfcPreferredPaymentChanged =
-                        pm.getPackagesHoldingPermissions(
-                        new String[] {android.Manifest.permission.NFC_PREFERRED_PAYMENT_INFO},
-                        0);
-                List<String> packageListNfcEvent = new ArrayList<String>();
-                for (int i = 0; i < packagesNfcEvents.size(); i++) {
-                    packageListNfcEvent.add(packagesNfcEvents.get(i).packageName);
-                }
-                mNfcEventInstalledPackages.put(uh.getIdentifier(), packageListNfcEvent);
-
-                List<String> packageListNfcPreferredPaymentChanged = new ArrayList<String>();
-                for (int i = 0; i < packagesNfcPreferredPaymentChanged.size(); i++) {
-                    packageListNfcPreferredPaymentChanged.add(
-                            packagesNfcPreferredPaymentChanged.get(i).packageName);
-                }
+                mNfcEventInstalledPackages.put(
+                        uh.getIdentifier(),
+                        getPackagesHoldingPermission(pm, NFC_TRANSACTION_EVENT));
                 mNfcPreferredPaymentChangedInstalledPackages.put(
-                        uh.getIdentifier(), packageListNfcPreferredPaymentChanged);
+                        uh.getIdentifier(),
+                        getPackagesHoldingPermission(pm, NFC_PREFERRED_PAYMENT_INFO));
             }
         }
     }
