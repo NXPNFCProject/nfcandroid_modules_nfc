3f961dbbc894c3f1883713cae2e8621f8c54f114
nxf24591 <nanjesh.s_1@nxp.com>
2025-07-11 17:58:45 +0530

    [artf1265062][25Q4_AUTO]: Fix a flanky test test_polling_frame_timestamp
    Need to wait for onServicesUpdated after startPollingFrameEmulatorActivity
    
    Bug: 409465275
    
    Test: atest CtsNfcHceMultiDeviceTestCases:test_polling_frame_timestamp -- --testparam pn532_serial_path=/dev/ttyUSB0
    
    Flag: EXEMPT bugfix

--- tests/cts/hostsidetests/multidevices/cts_nfc_hce_multi_device_test.py
+++ tests/cts/hostsidetests/multidevices/cts_nfc_hce_multi_device_test.py
@@ -1049,6 +1049,8 @@ class CtsNfcHceMultiDeviceTestCases(base_test.BaseTestClass):
             start_emulator_fun=self.emulator.nfc_emulator.startPollingFrameEmulatorActivity
         )
 
+        time.sleep(1) # Let NFC stack complete onServicesUpdated.
+
         timed_pn532 = TimedWrapper(self.pn532)
         testcases = [
             POLLING_FRAME_ON,
