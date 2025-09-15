26150ae64c2bf9c886b2feab08373483c3c1a3a8
nxf24591 <nanjesh.s_1@nxp.com>
2025-09-15 10:27:30 +0530

    Size of HCI buffer is only payload
    As per NCI specification
    
    Bug: 397142597
    
    Test: Compiles
    
    Flag: EXEMPT bug fix

--- libnfc-nci/src/nfa/hci/nfa_hci_utils.cc
+++ libnfc-nci/src/nfa/hci/nfa_hci_utils.cc
@@ -303,14 +303,12 @@ tNFA_STATUS nfa_hciu_send_msg(uint8_t pipe_id, uint8_t type,
   bool first_pkt = true;
   uint16_t data_len;
   tNFA_STATUS status = NFA_STATUS_OK;
+  uint16_t max_seg_hcp_pkt_size = nfa_hci_cb.buff_size;
   if ((msg_len != 0) && (p_msg == nullptr)) {
     LOG(ERROR) << StringPrintf("%s: msg_len is 0 and p_msg is null", __func__);
     return NFA_STATUS_FAILED;
   }
-  uint16_t max_seg_hcp_pkt_size;
-  if (nfa_hci_cb.buff_size > (NCI_DATA_HDR_SIZE + 2)) {
-    max_seg_hcp_pkt_size = nfa_hci_cb.buff_size - NCI_DATA_HDR_SIZE;
-  } else {
+  if (nfa_hci_cb.buff_size <= (NCI_DATA_HDR_SIZE + 2)) {
     android_errorWriteLog(0x534e4554, "124521372");
     return NFA_STATUS_NO_BUFFERS;
   }
