ec7f549d6fac260fa888c742b113070b963dc54b
nxf24591 <nanjesh.s_1@nxp.com>
2025-11-19 12:51:56 +0530

    [artf1307670][26Q2_AUTO]: nfc: Add a null check for mPollingLoopFilters.get before using it
    Check for null at this point to prevent a null object reference because the current logic doesn't yet have a better way to handle a null object.
    
    Bug: 444754388
    
    Test: manual
    
    Flag: EXEMPT BUGFIX

--- NfcNci/src/com/android/nfc/cardemulation/HostEmulationManager.java
+++ NfcNci/src/com/android/nfc/cardemulation/HostEmulationManager.java
@@ -654,8 +654,16 @@ public class HostEmulationManager {
                     if (DBG) Log.d(TAG, "onPollingLoopDetected: POLLING_LOOP_TYPE_UNKNOWN");
                     byte[] data = pollingFrame.getData();
                     String dataStr = HexFormat.of().formatHex(data).toUpperCase(Locale.ROOT);
-                    List<ApduServiceInfo> serviceInfos =
-                            mPollingLoopFilters.get(ActivityManager.getCurrentUser()).get(dataStr);
+                    Map<String, List<ApduServiceInfo>> MappingForUser =
+                            mPollingLoopFilters.get(ActivityManager.getCurrentUser());
+                    List<ApduServiceInfo> serviceInfos;
+                    if (MappingForUser != null) {
+                        serviceInfos = MappingForUser.get(dataStr);
+                    } else {
+                        Log.e(TAG, "MappingForUser is null, CurrentUser: "
+                                + ActivityManager.getCurrentUser());
+                        serviceInfos = null;
+                    }
                     Map<Pattern, List<ApduServiceInfo>> patternMappingForUser =
                             mPollingLoopPatternFilters.get(ActivityManager.getCurrentUser());
                     Set<Pattern> patternSet = patternMappingForUser.keySet();
