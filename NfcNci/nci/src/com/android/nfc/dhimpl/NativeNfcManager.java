f22b81213fb9dc477b8a00b22928513cdd00df84
nxf24591 <nanjesh.s_1@nxp.com>
2025-11-19 12:53:32 +0530

    [artf1307670][26Q2_AUTO]: nfc(metrics): Add a capability for reader mode annotation
    Bug: 398910589
    
    Test: N/A
    
    Flag: EXEMPT METRICS

--- NfcNci/nci/src/com/android/nfc/dhimpl/NativeNfcManager.java
+++ NfcNci/nci/src/com/android/nfc/dhimpl/NativeNfcManager.java
@@ -770,7 +770,8 @@ public class NativeNfcManager implements DeviceHost {
                 proprietaryCaps.isPollingFrameNotificationSupported(),
                 proprietaryCaps.isPowerSavingModeSupported(),
                 proprietaryCaps.isAutotransactPollingLoopFilterSupported(),
-                proprietaryCaps.getNumberOfExitFramesSupported());
+                proprietaryCaps.getNumberOfExitFramesSupported(),
+                proprietaryCaps.isReaderModeAnnotationSupported());
     }
 
     public void notifyObserveModeChanged(boolean enabled) {
