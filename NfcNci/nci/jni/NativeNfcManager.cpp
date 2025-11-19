b9c78d36f6c94115e3e0d95eea7221f959b69ce7
nxf24591 <nanjesh.s_1@nxp.com>
2025-11-19 12:53:29 +0530

    [artf1307670][26Q2_AUTO]: Set route update required flag before calling commitRouting
    Under race condition, there is chance the flag is cleared and the second
    
    commitRouting is skipped.
    
    Bug: 452382323
    
    Test: compiles
    
    Flag: EXEMPT BUGFIX

--- NfcNci/nci/jni/NativeNfcManager.cpp
+++ NfcNci/nci/jni/NativeNfcManager.cpp
@@ -1210,6 +1210,7 @@ static jint nfcManager_commitRouting(JNIEnv* e, jobject) {
     /*Update routing table only in Idle state.*/
     startRfDiscovery(false);
   }
+  RoutingManager::getInstance().setEeTechRouteUpdateRequired();
   jint commitStatus = RoutingManager::getInstance().commitRouting();
   startRfDiscovery(true);
   return commitStatus;
@@ -1981,8 +1982,10 @@ static void nfcManager_enableDiscovery(JNIEnv* e, jobject o,
   }
 
   // Checking if RT should be updated
-  if (!RoutingManager::getInstance().isRTUpdateOptimized())
+  if (!RoutingManager::getInstance().isRTUpdateOptimized()) {
+    RoutingManager::getInstance().setEeTechRouteUpdateRequired();
     RoutingManager::getInstance().commitRouting();
+  }
 
   // Actually start discovery.
   startRfDiscovery(true);
