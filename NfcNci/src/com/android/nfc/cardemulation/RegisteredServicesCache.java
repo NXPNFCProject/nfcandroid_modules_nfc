c80398fa9e24469b22db088f62a21f037d6d1db6
nxf24591 <nanjesh.s_1@nxp.com>
2025-11-19 12:53:59 +0530

    [artf1307670][26Q2_AUTO]: Skip adding NDEF-NFCEE AID for clone users
    Bug: 454164076
    
    Test: manual
    
    Flag: EXEMPT BUGFIX

--- NfcNci/src/com/android/nfc/cardemulation/RegisteredServicesCache.java
+++ NfcNci/src/com/android/nfc/cardemulation/RegisteredServicesCache.java
@@ -556,12 +556,14 @@ public class RegisteredServicesCache {
         }
 
         UserManager um = mContext.createContextAsUser(
-                UserHandle.of(ActivityManager.getCurrentUser()), /*flags=*/0)
+                UserHandle.of(userId), /*flags=*/0)
                 .getSystemService(UserManager.class);
         boolean isManagedProfile = um.isManagedProfile(userId);
+        boolean isCloneProfile = um.isCloneProfile();
         // Add NDEF-NFCEE AID - Only if NDEF-NFCEE feature supported
         // And only for user 0 to avoid adding several times (if multiple profiles)
-        if (!isManagedProfile && NfcService.getInstance().isNdefNfceefeatureEnabled()) {
+        if (!isManagedProfile && !isCloneProfile
+                && NfcService.getInstance().isNdefNfceefeatureEnabled()) {
             ResolveInfo ndefNfceeAppInfo = new ResolveInfo();
             ndefNfceeAppInfo.resolvePackageName = "NdefNfceeAidRoute";
             ndefNfceeAppInfo.serviceInfo = new ServiceInfo();
