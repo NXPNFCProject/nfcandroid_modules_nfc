e130e97b9a93d99341f2c5a8982fc7645048d593
Sangeetha Agarwal <sangeetha.agarwal@nxp.com>
2025-12-08 16:16:25 +0530

    Aligning with android-16.0.0_r4

--- NfcNci/nci/jni/RoutingManager.cpp
+++ NfcNci/nci/jni/RoutingManager.cpp
@@ -892,6 +892,7 @@ void RoutingManager::updateSystemCodeRoute(int route) {
   LOG(DEBUG) << StringPrintf("%s:  New default SC route=0x%x", fn, route);
   setEeInfoChangedFlag();
   mDefaultSysCodeRoute = route;
+  updateDefaultRoute();
 }
 
 /*******************************************************************************
