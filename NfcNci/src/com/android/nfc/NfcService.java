a574da3bb13dc2768e40885b91b24463c591b1a0
nxf24591 <nanjesh.s_1@nxp.com>
2026-01-05 15:22:24 +0530

    [artf1315196][26Q2_AUTO]: End presence checking if discovery stopped
    Bug: 444244445
    
    Cherrypick-From: https://partner-android-review.googlesource.com/q/commit:e5e09580d7be9a6293fc2b17f6baf103bafb0496
    
    Flag: EXEMPT BUGFIX
    
    Merged-In: I019a50d5c00fa18ecc82730cb1456b7f1efe52c1

--- NfcNci/src/com/android/nfc/NfcService.java
+++ NfcNci/src/com/android/nfc/NfcService.java
@@ -961,6 +961,9 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
         } catch (RemoteException e) {
             Log.e(TAG, "onRfDiscoveryEvent: e=", e);
         }
+        if (!isDiscoveryStarted) {
+            StopPresenceChecking(false);
+        }
     }
 
     @Override
@@ -1057,6 +1060,9 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
     public void onTagRfDiscovered(boolean discovered) {
         Log.d(TAG, "onTagRfDiscovered: " + discovered);
         executeOemOnTagConnectedCallback(discovered);
+        if(!discovered){
+            StopPresenceChecking(false);
+        }
     }
 
     final class ReaderModeParams {
