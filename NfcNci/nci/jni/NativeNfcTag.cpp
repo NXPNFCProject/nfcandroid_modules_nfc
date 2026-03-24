a5e5bd563b7d4a9b90e613fd3b471a1da4f77326
nxf65721 <shubham.salunkhe@nxp.com>
2026-03-24 10:34:08 +0530

    [artf1326623][26Q2_AUTO] Revert "Avoid being stuck in nativeNfcTag_doRead"
    This reverts commit 888a205784b79e0fe75ab158595cdf23c65b58de.
    
    Reason for revert: b/492415184
    
    Bug: 480037507
    Change-Id: Iec926a55f24a31d8ec89cbfb9d19e6de33a9b1f3

--- NfcNci/nci/jni/NativeNfcTag.cpp
+++ NfcNci/nci/jni/NativeNfcTag.cpp
@@ -335,10 +335,7 @@ static jbyteArray nativeNfcTag_doRead(JNIEnv* e, jobject) {
       SyncEventGuard g(sReadEvent);
       sIsReadingNdefMessage = true;
       status = NFA_RwReadNDef();
-      if (!sReadEvent.wait(10000)) {  // wait for NFA_READ_CPLT_EVT
-        LOG(ERROR) << StringPrintf("%s: Timeout reading tag NDEF content", __func__);
-        sReadDataLen = 0;
-      }
+      sReadEvent.wait();  // wait for NFA_READ_CPLT_EVT
     }
     sIsReadingNdefMessage = false;
 
